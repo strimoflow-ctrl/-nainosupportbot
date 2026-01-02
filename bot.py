98772ddaa21f5db22a39f2d4e364ab7d2131e2e8
strimoflow-ctrl <strimoflow@gmail.com>
2026-01-02 16:00:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -137,13 +137,13 @@ def handle_admin_reply(message):
         try:
             # Jawab user ko bhejo
             admin_name = message.from_user.first_name or "Admin"
-            bot.send_message(user_id, f"👨‍💻 {Admin}: {message.text}")
+            bot.send_message(user_id, f"👨‍💻 {admin_name}: {message.text}")
             
             # CONFIRMATION FIX: Hata kar sirf green tick bhejo (Reply to the admin's message)
-            bot.reply_to(message, "✓✓") 
+            bot.reply_to(message, "✅ Done") 
             
         except:
-            bot.reply_to(message, "××")
+            bot.reply_to(message, "❌ User Block The Bot")
 
 # --- WEB SERVER (Render ke liye) ---
 @app.route('/')
